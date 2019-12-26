d2e4aad6622c081127f85457e3f325699045a487
nimbusparis <mandrillon@gmail.com>
2019-12-26 21:01:05 +0100

    * Add Azure Key vault

--- TestAKVWithBot/Program.cs
+++ TestAKVWithBot/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -18,9 +19,29 @@ namespace TestAKVWithBot
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var builtConfig = config.Build();
+                        
+                    using var store = new X509Store(StoreName.My);
+                    store.Open(OpenFlags.ReadOnly);
+                    Console.WriteLine($"Number of certificates {store.Certificates.Count}");
+                    var certs = store.Certificates
+                        .Find(X509FindType.FindByThumbprint,
+                            builtConfig["AzureADCertThumbprint"], false);
+
+                    config.AddAzureKeyVault(
+                        $"https://{builtConfig["KeyVaultName"]}.vault.azure.net/",
+                        builtConfig["AzureADApplicationId"],
+                        certs.OfType<X509Certificate2>().Single());
+
+                    store.Close();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                
+        ;
     }
 }
